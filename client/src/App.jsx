4384dce38348f7bb71b53c000affc7dc32f85a34
kopildas <102140363+kopildas@users.noreply.github.com>
2024-04-29 19:59:38 +0600

    feat: page routes added in to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./page/Home/Home";
+import About from "./page/About/About";
+import Contact from "./page/Contact/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-  <>
-  
-  </>
+    <>
+      <Router>
+        {/* <Cursor scaling={scaling}/> */}
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
