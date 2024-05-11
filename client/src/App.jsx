620928ee824ab08565c23659a14bcd774f52d262
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-12 00:53:57 +0600

    feat: header and footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Home from "./page/Home/Home";
 import About from "./page/About/About";
 import Contact from "./page/Contact/Contact";
+import Header from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,12 +14,13 @@ function App() {
     <>
       <Router>
         {/* <Cursor scaling={scaling}/> */}
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer/>
       </Router>
     </>
   );
