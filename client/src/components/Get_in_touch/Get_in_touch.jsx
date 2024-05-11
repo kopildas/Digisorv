031ee36fe7ff789291095ee21da3cb81d6633e1b
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-12 00:58:23 +0600

    feat: get in touch added

---
+++ client/src/components/Get_in_touch/Get_in_touch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Get_in_touch = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900 flex items-center justify-center archivo-black-regular  text-zinc-300 flex-col'>
+        <p className='text-8xl'>LET’S WORK</p>
+        <p className='text-8xl'>TOGETHER</p>
+        <p className='text-xl'>Elevate your brand. Click for digital excellence!</p>
+    </div>
+  )
+}
