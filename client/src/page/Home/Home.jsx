8d8cdf7e697068a0e701e13cea6bc5ef89f2d8b7
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-12 01:01:45 +0600

    feat: home page added

--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
+import Services from '../../components/Services/Services'
+import Achieved from '../../components/Achieved/Achieved'
+import Customer_Review from '../../components/Customer_Review/Customer_Review'
+import { Get_in_touch } from '../../components/Get_in_touch/Get_in_touch'
 
 function Home() {
   return (
-    <div>
-      
+    <div className=''>
+      <Hero/>
+      <Services/>
+      <Achieved/>
+      <Customer_Review/>
+      <Get_in_touch/>
     </div>
   )
 }
