e59838deeb13ac6044dc4c150aa2f49d430a7f70
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-12 10:39:40 +0600

    fix: responsiveness to the get in touch design

--- client/src/components/Get_in_touch/Get_in_touch.jsx
+++ client/src/components/Get_in_touch/Get_in_touch.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Get_in_touch = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 flex items-center justify-center archivo-black-regular  text-zinc-300 flex-col'>
-        <p className='text-8xl'>LET’S WORK</p>
-        <p className='text-8xl'>TOGETHER</p>
-        <p className='text-xl'>Elevate your brand. Click for digital excellence!</p>
+        <p className='text-3xl md:text-8xl'>LET’S WORK</p>
+        <p className='text-3xl md:text-8xl'>TOGETHER</p>
+        <p className='text-sm md:text-xl'>Elevate your brand. Click for digital excellence!</p>
     </div>
   )
 }
