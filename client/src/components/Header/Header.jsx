9f3f17f91fb184aac45a6b48b3e316434d4008a8
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-17 18:17:51 +0600

    fix: header show and hide delay for pageTransition

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,16 +9,15 @@ export default function Header() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    
     const handleScroll = () => {
-      const storedScrollY = localStorage.getItem("scrollY");
-      const scrollY = window.scrollY;
-      
+      const storedScrollY = localStorage.getItem("scrollY") || 12;
+      const scrollY = window.scrollY - 8;
+
       if (storedScrollY > scrollY) {
-        localStorage.setItem("scrollY", scrollY); 
+        localStorage.setItem("scrollY", scrollY);
         setIsHeaderVisible(true); // Show header when scrolling up
       } else {
-        localStorage.setItem("scrollY", scrollY); 
+        localStorage.setItem("scrollY", scrollY);
         setIsHeaderVisible(false); // Hide header when scrolling down
       }
     };
@@ -28,79 +27,84 @@ export default function Header() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-
   const toggle = () => {
     setToggle_menu(!toggle_menu);
   };
 
-  return (
-    <nav className={`bg-zinc-00 fixed w-full flex items-center justify-between px-10 py-5 order-b z-50 transition duration-300 ease-in-out ${isHeaderVisible ? '' : 'opacity-0 transform -translate-y-full'}`}>
-      {toggle_menu && <Toggle_menu toggle={toggle} />}
-      <div className="w-full flex items-center justify-between bg-zinc-600 px-5 md:px-10 py-4 rounded-3xl">
-        <div className="">
-          <p
-            onClick={() => {
-              navigate("/");
-            }}
-            
-            className="w-32 lg:ml-0 cursor-pointer text-4xl text-white"
-          >Digisorv</p>
+  return 
+      <nav
+        className={`bg-zinc-00 fixed w-full flex items-center justify-between px-10 py-5 order-b z-50 transition duration-300 ease-in-out ${
+          isHeaderVisible ? "" : "opacity-0 transform -translate-y-full"
+        }`}
+      >
+        {toggle_menu && <Toggle_menu toggle={toggle} />}
+        <div className="w-full flex items-center justify-between bg-zinc-600 px-5 md:px-10 py-4 rounded-3xl">
+          <div className="">
+            <p
+              onClick={() => {
+                navigate("/");
+              }}
+              className="w-32 lg:ml-0 cursor-pointer text-4xl text-white"
+            >
+              Digisorv
+            </p>
+          </div>
+
+          <div className=" gap-10 hidden lg:flex">
+            <p
+              className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </p>
+            <p
+              className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </p>
+            <p
+              className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
+              onClick={() => {
+                navigate("/services");
+              }}
+            >
+              Services
+            </p>
+            <p
+              className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
+              onClick={() => {
+                navigate("/blog");
+              }}
+            >
+              Blog
+            </p>
+          </div>
+
+          <div className="hidden lg:flex gap-5 border border-shades-3 rounded-3xl font-semibold text-white backdrop-blur-[40px] px-10 py-2 text-shades-8 text-lg text-font">
+            <p
+              className="cursor-pointer"
+              onClick={() => {
+                navigate("/contact");
+              }}
+            >
+              Contact us
+            </p>
+          </div>
+
+          <div className=" lg:hidden flex">
+            <p
+              className="text-4xl text-shades-8 cursor-pointer"
+              onClick={toggle}
+            >
+              <CgMenuRight />
+            </p>
+          </div>
         </div>
-
-        <div className=" gap-10 hidden lg:flex">
-          <p
-            className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            Home
-          </p>
-          <p
-            className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
-            onClick={() => {
-              navigate("/about");
-            }}
-          >
-            About
-          </p>
-          <p
-            className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
-            onClick={() => {
-              navigate("/services");
-            }}
-          >
-            Services
-          </p>
-          <p
-            className="cursor-pointer text-white font-semibold text-2xl hover:text-blue-300 transition-all duration-200 ease-in-out"
-            onClick={() => {
-              navigate("/blog");
-            }}
-          >
-            Blog
-          </p>
-        </div>
-
-        <div className="hidden lg:flex gap-5 border border-shades-3 rounded-3xl font-semibold text-white backdrop-blur-[40px] px-10 py-2 text-shades-8 text-lg text-font">
-          <p
-            className="cursor-pointer"
-            onClick={() => {
-              navigate("/contact");
-            }}
-          >
-            Contact us
-          </p>
-        </div>
-
-        <div className=' lg:hidden flex'>
-          <p className='text-4xl text-shades-8 cursor-pointer' onClick={toggle}><CgMenuRight /></p>
-          
-
-        </div>
-
-      </div>
-      
-    </nav>
+      </nav>
   );
 }
