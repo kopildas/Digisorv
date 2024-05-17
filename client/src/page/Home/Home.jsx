4cea0bf991574c6551db6f2075faffac0a62ba86
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-17 18:23:30 +0600

    feat: added page transition effect on the page

--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import Services from '../../components/Services/Services'
-import Achieved from '../../components/Achieved/Achieved'
-import Customer_Review from '../../components/Customer_Review/Customer_Review'
-import { Get_in_touch } from '../../components/Get_in_touch/Get_in_touch'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import Services from "../../components/Services/Services";
+import Achieved from "../../components/Achieved/Achieved";
+import Customer_Review from "../../components/Customer_Review/Customer_Review";
+import { Get_in_touch } from "../../components/Get_in_touch/Get_in_touch";
+import Transition from "../../components/Page_transition/Transition";
 
 function Home() {
   return (
-    <div className=''>
-      <Hero/>
-      <Services/>
-      <Achieved/>
-      <Customer_Review/>
-      <Get_in_touch/>
-    </div>
-  )
+    <Transition>
+      <div className="">
+        <Hero />
+        <Services />
+        <Achieved />
+        <Customer_Review />
+        <Get_in_touch />
+      </div>
+    </Transition>
+  );
 }
 
-export default Home
+export default Home;
---
+++ client/src/page/Services/Services_page.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Transition from '../../components/Page_transition/Transition'
+
+function Services_page() {
+  return (
+<Transition>
+      <div className="w-full h-screen flex-col place-content-center">
+        <div>
+          <p className="text-2xl">this is service page</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero velit
+            tempora, nesciunt odio saepe amet repellat neque quasi facere quod
+            quos, ipsum consectetur aut corrupti minus esse labore libero harum.
+          </p>
+        </div>
+        <div>
+          <p className="text-2xl">this is service page</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero velit
+            tempora, nesciunt odio saepe amet repellat neque quasi facere quod
+            quos, ipsum consectetur aut corrupti minus esse labore libero harum.
+          </p>
+        </div>
+        <div>
+          <p className="text-2xl">this is service page</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero velit
+            tempora, nesciunt odio saepe amet repellat neque quasi facere quod
+            quos, ipsum consectetur aut corrupti minus esse labore libero harum.
+          </p>
+        </div>
+      </div>
+    </Transition>
+  )
+}
+
+export default Services_page
