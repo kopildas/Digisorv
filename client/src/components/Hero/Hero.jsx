ea62be52016c0b0b17fb9960cd1a3c5014b440b2
kopildas <102140363+kopildas@users.noreply.github.com>
2024-05-31 23:00:46 +0600

    feat: round slogan text added

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import RoundScrolling from "./RoundScrolling";
 import { motion } from "framer-motion";
 import { fadeIn, springAnimation } from "../../variants.js";
+import { IoIosArrowRoundDown } from "react-icons/io";
 
 function Hero() {
   return (
@@ -21,28 +22,39 @@ function Hero() {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.7 }}
-          className="archivo-black-regular mb-10 text-slate-300 text-4xl md:text-6xl lg:text-8xl lg:leading-[5rem]"
+          className="archivo-black-regular mb-10 text-slate-300 text-4xl md:text-6xl lg:text-8xl lg:leading-[6.5rem]"
         >
-          Welcome to our <br /> digital Agency where <br /> we meet expertise.
+          Welcome to our <br /> digital <span className="bg-blue-500"><motion.span
+          variants={fadeIn("up", 0.8)}
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.9 }} >A</motion.span></span>gency where <br /> we meet expertise.
         </motion.p>
         <div className="flex flex-col md:flex-row  md:gap-10 lg:gap-20">
-          <div className="lg:w-[180px] md:w-[150px] hidden md:flex md:h-[150px] lg:h-[180px]  rounded-full bg-slate-500"></div>
+          {/* round circle with text */}
+          <div className="lg:w-[135px] md:w-[120px] hidden md:flex items-center justify-center md:h-[120px] lg:h-[135px]  rounded-full bg-slate-300 relative ">
+            <img src="https://i.ibb.co/2Zrjw1H/inkpx-curved-text.png" alt="" className="text absolute p-2"/>
+            <div className="w-[5rem] h-[5rem] bg-blue-500 rounded-full flex items-center justify-center border border-zinc-800">
+                <IoIosArrowRoundDown className="text-6xl text-zinc-700 "/>
+            </div>
+          </div>
           <motion.div
             variants={fadeIn("right", 0.7)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.9 }}
-            className="md:w-[50%] text-xs md:text-base lg:text-xl bg-fuchsia00 text-slate-400"
+            className="md:w-[50%] text-xs md:text-base lg:text-xl  bg-fuchsia-30 text-slate-400"
           >
             <p>
-              Welcome To Digisorv Where Innovation Meets Digital Excellence. As
-              A Leading Digital Agency, We Specialize In Crafting Compelling
-              Online Experiences That Drive Results. Our Team Of Experts Is
-              Dedicated To Navigating The Dynamic Landscape Of The Digital
-              World.
+            At Digisorv, we transform your online presence with tailored web development, engaging content, and effective digital marketing strategies. Our passionate team is committed to helping your business thrive in the digital world.
             </p>
           </motion.div>
-          <div className="w-[100px] h-[100px] mt-5 block md:hidden rounded-full bg-slate-500"></div>
+          <div className="w-[100px] h-[100px] mt-5  md:hidden rounded-full bg-slate-300 relative flex items-center justify-center">
+          <img src="https://i.ibb.co/2Zrjw1H/inkpx-curved-text.png" alt="" className="text absolute p-1"/>
+            <div className="w-[3.5rem] h-[3.5rem] bg-blue-500 rounded-full flex items-center justify-center border border-zinc-800">
+                <IoIosArrowRoundDown className="text-4xl text-zinc-700 "/>
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full md:h-96 bg-zinc-00 absolute z-10  bottom-0 flex flex-col md:flex-row overflow-hidden">
