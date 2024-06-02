1548ff135e84c0c9f2c1e51b78d3101dd9f00b5f
kopildas <102140363+kopildas@users.noreply.github.com>
2024-06-02 16:38:56 +0600

    fix: color changed orange to blue

--- client/src/components/Achieved/Achieved.jsx
+++ client/src/components/Achieved/Achieved.jsx
@@ -7,7 +7,7 @@ function Achieved() {
     <div className='flex flex-col h-auto bg-zinc-400 px-5 lg:px-16 py-20 '>
       <div className='bg-zinc-700 rounded-3xl md:rounded-[4rem] w-full h-full flex flex-col mt-[12rem] md:mt-10 lg:mt-10'>
         <div className='flex items-center justify-center mt-20'>
-          <p className='archivo-black-regular text-zinc-300 font-extrabold lg:text-left text-center text-5xl md:text-7xl'>What we <span className='text-orange-400'><i>achieved</i></span></p>
+          <p className='archivo-black-regular text-zinc-300 font-extrabold lg:text-left text-center text-5xl md:text-7xl'>What we <span className='text-blue-400'><i>achieved</i></span></p>
         </div>
         <div className='flex flex-col lg:flex-row w-full h-auto p-5 md:p-20 mt-5 lg:mt-0 gap-5 lg:gap-0'>
           <div className='lg:w-1/3 h-96 border lg:border-r-0 rounded-3xl lg:rounded-none lg:rounded-bl-3xl lg:rounded-tl-3xl flex flex-col items-center justify-center lg:items-start lg:justify-start px-5 md:px-10  py-14 bg-red-00'>
